3feec559d05f75dc51c69051396c73259ee711d6
Sainath <saisainath777@gmail.com>
2022-06-03 11:11:18 +0530

    basic consitional learning completed

---
+++ Day24/main.js
@@ -0,0 +1,25 @@
+// if(5 > 2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// if(9 < 13) {
+//     console.log('9 is greater than 13');
+// }
+
+// let shoppingDone = false;
+// let childAllowance;
+
+// if (shoppingDone === true) {
+//     childAllowance = 10;
+// } else {
+//     childAllowance = 5;
+// }
+// console.log(childAllowance);
+
+// if (childAllowance === 5) {
+//     console.log('child is dissapointed');
+// } else {
+//     console.log('child is happy');
+// }
